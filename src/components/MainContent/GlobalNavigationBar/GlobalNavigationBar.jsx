5ac21d911b01ff22232bb9529f4237d22e01d9c5
dkr-sjr <rkdrjs4637@naver.com>
2025-11-12 14:47:08 +0900

    refactor(GlobalNavigationBar): styled-component 적용

--- src/components/MainContent/GlobalNavigationBar/GlobalNavigationBar.jsx
+++ src/components/MainContent/GlobalNavigationBar/GlobalNavigationBar.jsx
@@ -1,12 +1,49 @@
-import styles from './GlobalNavigationBar.module.css';
+import styled from 'styled-components';
+
+const GNBContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+
+  padding: 0 16px;
+
+  background-color: var(--primary-color);
+`;
+
+const GNBTitle = styled.h1`
+  color: #fcfcfd;
+  
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 600;
+`;
+
+const GNBButton = styled.button`
+  height: 40px;
+
+  border: none;
+  border-radius: 8px;
+  background: transparent;
+
+  font-size: 24px;
+  cursor: pointer;
+
+  img {
+    display: block;
+    width: 40px;
+    height: 40px;
+    object-fit: contain;
+  }
+`;
 
 export default function GlobalNavigationBar({ showAddRestaurantModal }) {
   return (
-    <header className={styles.gnb}>
-      <h1 className={`${styles.gnbTitle} text-title`}>점심 뭐 먹지</h1>
-      <button type="button" className={styles.gnbButton} aria-label="음식점 추가" onClick={showAddRestaurantModal}>
+    <GNBContainer>
+      <GNBTitle>점심 뭐 먹지</GNBTitle>
+      <GNBButton type="button" aria-label="음식점 추가" onClick={showAddRestaurantModal}>
         <img src="templates/add-button.png" alt="음식점 추가" />
-      </button>
-    </header>
+      </GNBButton>
+    </GNBContainer>
   );
 }
