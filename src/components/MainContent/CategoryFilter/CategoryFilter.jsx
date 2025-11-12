7f19551d21b7152948c4ab0b001bc561c10deb2c
dkr-sjr <rkdrjs4637@naver.com>
2025-11-12 15:12:27 +0900

    refactor(CategoryFilter): styled-component 적용

--- src/components/MainContent/CategoryFilter/CategoryFilter.jsx
+++ src/components/MainContent/CategoryFilter/CategoryFilter.jsx
@@ -1,5 +1,26 @@
-import styles from './CategoryFilter.module.css';
+import styled from 'styled-components';
 import categoryList from '../../../Data/categoryList';
+
+const CategoryFilterContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0 16px;
+  margin-top: 24px;
+
+  select{
+    height: 44px;
+    min-width: 125px;
+
+    border: 1px solid #d0d5dd;
+    border-radius: 8px;
+    background: transparent;
+
+    font-size: 16px;
+    
+    padding: 8px;
+  }
+`;
 
 export default function CategoryFilter({ category, onChangeCategory }) {
   const optionList = categoryList.map((value) => (
@@ -7,17 +28,16 @@ export default function CategoryFilter({ category, onChangeCategory }) {
   ));
 
   return (
-    <section className={styles.restaurantFilterContainer}>
+    <CategoryFilterContainer>
       <select
         name="category"
         id="category-filter"
         value={category}
         onChange={(e) => onChangeCategory(e.target.value)}
-        className={styles.restaurantFilter}
         aria-label="음식점 카테고리 필터"
       >
         {optionList}
       </select>
-    </section>
+    </CategoryFilterContainer>
   );
 }
